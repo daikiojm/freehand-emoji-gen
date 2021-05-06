114f04cbfaae531296f5fa9c9d31efeec1d38e60
daikiojm <daiking.ca2@gmail.com>
2021-05-06 18:47:49 +0900

    fix: spec

--- test/Index.spec.js
+++ test/Index.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Index from '@/pages/index.vue'
 
-describe('Logo', () => {
+describe('Index', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Index)
     expect(wrapper.vm).toBeTruthy()
   })
 })
