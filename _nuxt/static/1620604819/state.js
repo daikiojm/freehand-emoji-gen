07f3481fdde7229775bf89d214f2470179235a82
daikiojm <daikiojm@users.noreply.github.com>
2021-05-10 00:00:20 +0000

    deploy: 7cd68d1331cb2edb57fdf9103838429a9ae307d0

---
+++ _nuxt/static/1620604819/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffreehand-emoji-gen\u002F_nuxt\u002Fstatic\u002F1620604819",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffreehand-emoji-gen\u002F",assetsPath:"\u002Ffreehand-emoji-gen\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
