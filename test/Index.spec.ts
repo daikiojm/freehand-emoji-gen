af314597fe567059f214ab339b6af9cffa58cd24
daikiojm <daiking.ca2@gmail.com>
2021-05-11 00:34:21 +0900

    test: skip useless spec

--- test/Index.spec.ts
+++ test/Index.spec.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 
 import Index from '@/pages/index.vue'
 
-describe('Index', () => {
+describe.skip('Index', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Index)
     expect(wrapper.vm).toBeTruthy()
