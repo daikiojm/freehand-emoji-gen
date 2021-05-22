87cef2f8d52aefc168c4f89380c30d9a145e0ac5
daikiojm <daikiojm@users.noreply.github.com>
2021-05-22 06:56:40 +0000

    deploy: c8f0138c66e09e1d72a75ab80e703bd452c4c1b5

--- _nuxt/f8a285e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{449:function(t,e,n){"use strict";n.r(e);var l=n(24),c=Object(l.b)({}),o=n(58),component=Object(o.a)(c,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("label",{staticClass:"text-caption"},[t._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
