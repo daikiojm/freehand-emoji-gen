dfcc2c83c4925daf6c4420a4c72cc1fd71dfe212
daikiojm <daiking.ca2@gmail.com>
2021-05-25 23:13:36 +0900

    fix: improve canvas event handler

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -1,17 +1,22 @@
 import { useStore } from '~/store'
 
+interface PointerEventEx extends PointerEvent {
+  currentTarget: Element
+}
+
 export const useEvents = () => {
   const { setCurrentMark, updateCurrentMark, endMark } = useStore()
 
-  const handlePointerDown = (e: PointerEvent) => {
+  const handlePointerDown = (e: PointerEventEx) => {
     e.preventDefault()
+    e.currentTarget.setPointerCapture(e.pointerId)
     setCurrentMark({
       type: e.pointerType,
       points: [[e.offsetX, e.offsetY, e.pressure]],
     })
   }
 
-  const handlePointerMove = (e: PointerEvent) => {
+  const handlePointerMove = (e: PointerEventEx) => {
     e.preventDefault()
     if (e.buttons === 1) {
       updateCurrentMark({
@@ -21,8 +26,9 @@ export const useEvents = () => {
     }
   }
 
-  const handlePointerUp = (e: PointerEvent) => {
+  const handlePointerUp = (e: PointerEventEx) => {
     e.preventDefault()
+    e.currentTarget.releasePointerCapture(e.pointerId)
     endMark()
   }
 
